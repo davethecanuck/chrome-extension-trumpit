50561881a4287863e9445cce45e318e84a2c9b53
David Sutherland <davethecanuck@gmail.com>
2020-08-30 20:35:39 -0600

    Added extension icon and updated release script to include it

--- create_zip.sh
+++ create_zip.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
+rm -rf trumpit_release.zip
 mkdir trumpit_release
 cp -p *.json trumpit_release
 cp -p *.js trumpit_release
+cp -p *.png trumpit_release
 cp -pr data trumpit_release
 cp -pr images trumpit_release
 zip trumpit_release trumpit_release
